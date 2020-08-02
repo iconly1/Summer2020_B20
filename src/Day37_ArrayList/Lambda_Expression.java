cf36325288487ee2ccd493ed1695475951b356d9
iconly1 <iconly1@gmail.com>
2020-08-02 15:10:50 -0400

    Update 4 Arraylists

---
+++ src/Day37_ArrayList/Lambda_Expression.java
@@ -0,0 +1,56 @@
+package Day37_ArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.function.Predicate;
+
+public class Lambda_Expression {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.addAll(Arrays.asList(1,1,1,2,3,4,4,4,5,6,7,8,9));
+
+        //Predicate<Integer> lessThan5 = p->p<5;
+
+        list.removeIf(p->p<5);
+
+        System.out.println(list);
+        System.out.println("=============================");
+        Predicate<Integer> oddnumber = p -> p % 2 != 0;
+        ArrayList<Integer> list2 = new ArrayList<>();
+        for (int i = 0; i<=100; i++){
+            list2.add(i);
+        }
+       //list2.removeIf(oddnumber);//removes odd numbers
+        list2.removeIf(p-> p %2 ==0);// removes all even numbers.
+        System.out.println(list2);
+        System.out.println("=====================");
+
+        ArrayList<String> names = new ArrayList<>();
+        names.addAll(Arrays.asList("Mary","Monica","Mehray","Musti","Sumeyra","Hasan","Beril"));
+
+        names.removeIf(each -> each.startsWith("M") && each.endsWith("y"));
+        //              ^can be any name (->)=Predicate
+        System.out.println(names);
+        System.out.println("=========================================");
+
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        nums.addAll(Arrays.asList(1,1,1,2,2,2,3,3,3,4,5,6,7,8,8,9));
+
+        nums.removeIf(p -> Collections.frequency(nums,p)!=1);
+                        // for the elements that are not unique
+        System.out.println(nums);
+        System.out.println("=============================================================");
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/Day37_ArrayList/Remove_Digits_Letters.java
@@ -0,0 +1,10 @@
+package Day37_ArrayList;
+
+import java.util.ArrayList;
+
+public class Remove_Digits_Letters {
+    public static void main(String[] args) {
+        ArrayList<Character> chars = new ArrayList<>();
+
+    }
+}
