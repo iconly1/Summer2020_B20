08907d81d25739121339a396072ec2830d079e31
iconly1 <iconly1@gmail.com>
2020-08-04 20:13:31 -0400

    Update 4 Arraylists Day38 update 1

--- src/rep.java
+++ src/rep.java
@@ -6,43 +6,50 @@ public class rep {
         String word = scan.next();
         int length = word.length();
 
-        if (length ==10){
-            System.out.println(""+word.charAt(0)+word.charAt(1)+word.charAt(2)+word.charAt(3)+word.charAt(4)
-                    +word.charAt(0)+word.charAt(1)+word.charAt(2)+word.charAt(3)+word.charAt(4));
-        }else if (length==9){
-            System.out.println(""+word.charAt(0)+word.charAt(1)+word.charAt(2)+word.charAt(3)
-                    +word.charAt(0)+word.charAt(1)+word.charAt(2)+word.charAt(3));
-        }else if(length==8){
-            System.out.println(""+word.charAt(0)+word.charAt(1)+word.charAt(2)+word.charAt(3)
-                    +word.charAt(0)+word.charAt(1)+word.charAt(2)+word.charAt(3));
-        }else if (length==7){
-            System.out.println(""+word.charAt(0)+word.charAt(1)+word.charAt(2)
-                    +word.charAt(0)+word.charAt(1)+word.charAt(2));
-        }else if (length==6){
-            System.out.println(""+word.charAt(0)+word.charAt(1)+word.charAt(2)+word.charAt(3)
-                    +word.charAt(0)+word.charAt(1)+word.charAt(2)+word.charAt(3));
-        } else if (length==5){
-            System.out.println(""+word.charAt(0)+word.charAt(1)
-                    +word.charAt(0)+word.charAt(1));
-        }else if (length==4){
-            System.out.println(""+word.charAt(0)+word.charAt(1)+word.charAt(0)+word.charAt(1));
-        }else if (length==3){
-            System.out.println(""+word.charAt(0)+word.charAt(1)+word.charAt(0));
-        }else if (length==2){
-            System.out.println(""+word.charAt(0));
-        }else{
-            System.out.println(""+word.charAt(0));
+        if (length == 10) {
+            System.out.println("" + word.charAt(0) + word.charAt(1) + word.charAt(2) + word.charAt(3) + word.charAt(4)
+                    + word.charAt(0) + word.charAt(1) + word.charAt(2) + word.charAt(3) + word.charAt(4));
+        } else if (length == 9) {
+            System.out.println("" + word.charAt(0) + word.charAt(1) + word.charAt(2) + word.charAt(3)
+                    + word.charAt(0) + word.charAt(1) + word.charAt(2) + word.charAt(3));
+        } else if (length == 8) {
+            System.out.println("" + word.charAt(0) + word.charAt(1) + word.charAt(2) + word.charAt(3)
+                    + word.charAt(0) + word.charAt(1) + word.charAt(2) + word.charAt(3));
+        } else if (length == 7) {
+            System.out.println("" + word.charAt(0) + word.charAt(1) + word.charAt(2)
+                    + word.charAt(0) + word.charAt(1) + word.charAt(2));
+        } else if (length == 6) {
+            System.out.println("" + word.charAt(0) + word.charAt(1) + word.charAt(2) + word.charAt(3)
+                    + word.charAt(0) + word.charAt(1) + word.charAt(2) + word.charAt(3));
+        } else if (length == 5) {
+            System.out.println("" + word.charAt(0) + word.charAt(1)
+                    + word.charAt(0) + word.charAt(1));
+        } else if (length == 4) {
+            System.out.println("" + word.charAt(0) + word.charAt(1) + word.charAt(0) + word.charAt(1));
+        } else if (length == 3) {
+            System.out.println("" + word.charAt(0) + word.charAt(1) + word.charAt(0));
+        } else if (length == 2) {
+            System.out.println("" + word.charAt(0));
+        } else {
+            System.out.println("" + word.charAt(0));
         }
 
 
 
 
-
-
-
-
-
-
-
     }
+    public static int[] mergR ( int[] a, int[] b){
+        int count = 0;
+        int[] c = new int[a.length - 1 + b.length - 1];
+        for (int i = 0; i <= a.length-1; i++) {
+            c[i] += a[i];
+            count++;
+        }
+        for (int i = count; i <= b.length-1; i++) {
+            c[i] += b[i];
+        }
+        return c;
+    }
+
 }
+
